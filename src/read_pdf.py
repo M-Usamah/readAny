dd5e971b47d7e327dabd9651c6be1ff5cb4c6183
M-Usamah <mohammad.usamah127@hotmail.com>
2024-10-11 00:13:31 +0500

    Read PDF

--- src/read_pdf.py
+++ src/read_pdf.py
@@ -4,13 +4,16 @@ def pdf_file(file):
     return PdfReader(file)
 
 def total_pages(file) -> int:
-    Pages = pdf_file(file)
-    return len(Pages.pages)
-    
-def read_single_page_text(pages,page_num) -> str:
-    file = pdf_file(pages)
-    page = file.pages[page_num]
-    return page_num, page.extract_text()
-    
+    reader = pdf_file(file)
+    return len(reader.pages)
+
+def read_single_page_text(file, page_num) -> tuple[int, str]:
+    reader = pdf_file(file)
+    if 0 <= page_num < len(reader.pages):
+        page = reader.pages[page_num]
+        return page_num, page.extract_text()
+    else:
+        raise ValueError(f"Page number {page_num} is out of range")
+
 if __name__ == "__main__":
-    print(read_single_page_text('../Artificial Intelligence A Modern Approach.pdf',22)[1])
+    print(read_single_page_text('../Artificial Intelligence A Modern Approach.pdf', 22)[1])
